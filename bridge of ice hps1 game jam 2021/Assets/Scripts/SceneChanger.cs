d6b27c761482f60d3c7322da41a0e94fd75e9278
DESKTOP-0UMACHJ\Ryan <RyanWork26@gmail.com>
2021-08-22 15:28:25 -0400

    Initial Cinematic setup, need fine tuning

---
+++ bridge of ice hps1 game jam 2021/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+
+    [SerializeField] Scene scene;
+    public void ChangeScene(string scenename)
+    {
+        SceneManager.LoadScene(scenename);
+    }
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(scene.name);
+    }
+
+
+
+    void ChangeSceneAsync(string scenename)
+    {
+
+
+    }
+}
